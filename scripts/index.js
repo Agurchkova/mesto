0bc799d8508f24aeabe9f3fe60f95a3b6e0534e2
Agurchkova <copyart@mail.ru>
2022-11-17 20:50:14 +0300

    1711 js

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+console.log(popupOpenButtonElement);
